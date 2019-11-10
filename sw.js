61cdb4fe940c5d424a3cf716ae5fa64376f0e021
Andi Irsandi <Andi.irsandi765@gmail.com>
2019-11-10 14:07:20 +0800

    update sw.js
    bagian serviceworker, gue nambahin script buat nangkap data API agar bisa ditampilin secara offline

--- sw.js
+++ sw.js
@@ -4,13 +4,15 @@ var urlsToCache = [
     '/index.html',
     '/js/jquery-3.4.1.min.js',
     '/js/main.js',
-    '/image/jam.png'
+    '/image/jam.png',
+    '/fallback.json'
 
 ];
 
 self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
+
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('in install service worker Opened cache');
             return cache.addAll(urlsToCache);
@@ -19,15 +21,36 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
+
+    var request = event.request
+    var url = new URL(request.url)
+
+
+    // pisah request API dan Internal
+    if (url.origin === location.origin) {
+        event.respondWith(
+            caches.match(request).then(function (response) {
+                return response || fetch(request)
+
+            })
+
+        )
+
+    } else {
+        event.respondWith(
+            caches.open('products-cache').then(function (cache) {
+                return fetch(request).then(function (liveResponse) {
+                    cache.put(request, liveResponse.clone())
+                    return liveResponse
+                }).catch(function () {
+                    return caches.match(request).then(function (response) {
+                        if (response) return response
+                        return caches.match('/fallback.json')
+                    })
+                })
+            })
+        )
+    }
 });
 
 self.addEventListener('activate', function (event) {
